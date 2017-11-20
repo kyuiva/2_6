78a921a3930e2c55b245339f65f2a3fa0fac8ad9
kyuiva <sow11@inbox.ru>
2017-11-20 11:46:57 +0300

    3

--- etc/hello.py
+++ etc/hello.py
@@ -1,9 +1,9 @@
 CONFIG = {                                                                      
-    # 'mode': 'wsgi',                                                           
+    'mode': 'wsgi',                                                           
     'working_dir': '/home/box/web/hello.py',                                           
-    # 'python': '/usr/bin/python',                                              
+    'python': '/usr/bin/python',                                              
     'args': (                                                                   
-        # '--bind=0.0.0.0:8080',                                              
+        '--bind=0.0.0.0:8080',                                              
         '--workers=16',                                                         
         '--timeout=60',                                                         
         'hello.app',                                                           
